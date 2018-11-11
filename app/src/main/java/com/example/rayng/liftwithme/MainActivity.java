d5d3f483cbe817ec0c17dcfac387316c4b7db210
ray <raynguyen64@gmail.com>
2018-11-11 15:11:23 -0800

    This is second commit for this project. Updates: (1) TextView displaying the PRs has been broken into three individual Text views; squatSummary, benchSummary, and deadSummary. (2) Main.java is able to write to the three TextView summaries.
    ToDo:
    (1) Add EditView that pulls user input and stores values into variables that will be written into the summary TextViews.

--- app/src/main/java/com/example/rayng/liftwithme/MainActivity.java
+++ app/src/main/java/com/example/rayng/liftwithme/MainActivity.java
@@ -12,16 +12,34 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    long squatPR, benchPR, deadPR;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView textView = (TextView) findViewById(R.id.LiftsSummary);
-        textView.setText("You did it");
+        TextView textView = findViewById(R.id.liftsSummary);
+        TextView squatView = findViewById(R.id.squatSummary);
+        TextView benchView = findViewById(R.id.benchSummary);
+        TextView deadView = findViewById(R.id.deadSummary);
+
+        squatPR = 245;//You will have to check the server for the correct value here.
+        benchPR = 145;
+        deadPR = 315;
+
+        String squat_message = "Squat 1RM is : " + squatPR;
+        String bench_message = "Benchpress 1RM is : " + benchPR;
+        String dead_message = "Deadlift 1RM is : " + deadPR;
+
+        textView.setText(R.string.summary_title);
+        squatView.setText(squat_message);
+        benchView.setText(bench_message);
+        deadView.setText(dead_message);
+
     }
 
-    @Override
+    /*@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
@@ -41,6 +59,6 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
+    }*/
 
 }
